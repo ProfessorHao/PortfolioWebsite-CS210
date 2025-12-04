42bf1a1118a037e9e0335796d5f0417ecdd69bf2
Roman Salazar <Rasalazar@nic.edu>
2025-12-04 10:00:22 -0800

    new

---
+++ functions.js
@@ -0,0 +1,7 @@
+function showExtra() {
+    document.getElementById("extraInfo").style.display = "block";
+}
+
+function hideExtra() {
+    document.getElementById("extraInfo").style.display = "none";
+}
